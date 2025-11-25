b1723ef9c2dbabe0247f7f577ec871786cbb19d0
Cuong Nguyen <cuongnv243@ghtk.co>
2025-11-25 13:33:04 +0700

    refactor: remove certificate pinning for API requests
    - Removed custom ServerTrustManager and certificate file.
    - Now using the default Alamofire Session configuration.
    - This simplifies the networking layer and avoids certificate management.

--- MinWallet/Data/Network/Restful/APIRouter/NetworkManager.swift
+++ MinWallet/Data/Network/Restful/APIRouter/NetworkManager.swift
@@ -6,22 +6,6 @@ final class NetworkManager {
     let session: Session
     
     private init() {
-        /*TODO: update cer with command
-        openssl s_client -connect ios-api.minswap.org:443 -showcerts < /dev/null \
-        | openssl x509 -outform DER > ios-api.cer
-         */
-        let evaluators: [String: ServerTrustEvaluating] = [
-            "ios-api.minswap.org": PublicKeysTrustEvaluator(
-                performDefaultValidation: true,
-                validateHost: true
-            )
-        ]
-
-        let serverTrustManager = ServerTrustManager(
-            allHostsMustBeEvaluated: true,
-            evaluators: evaluators
-        )
-        
-        session = Session(serverTrustManager: serverTrustManager)
+        session = .default
     }
 }
